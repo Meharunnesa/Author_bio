7fbd7ae3485edf3e61e4ffe91a57bcd9d94ea7f6
Meharunnesa <meharunnesab28@gmail.com>
2025-04-06 17:02:25 +0600

    all author data retrived

--- index.php
+++ index.php
@@ -9,14 +9,51 @@
  * Text Domain:       AB
 */
 
+function ab_styles(){
+
+    wp_enqueue_style( 'custom-style', plugin_dir_url(__FILE__) . 'style.css', array(), '1.0.0', 'all' );
+   
+}
+
+add_action('admin_enqueue_scripts', 'ab_styles');
+
 function display_author_bio($content){
    
     global $post;
 
     if (is_single() && !empty($post)) {
         $author_name = get_the_author_meta('display_name', $post->post_author);
-        $author_bio = '<p><strong>Author: ' . esc_html($author_name) . '</strong></p>';
-        return $content . $author_bio;
+        $author_mail = get_the_author_meta('email', $post->post_author);
+        $author_url = get_the_author_meta('url', $post->post_author);
+        $author_description = get_the_author_meta('description', $post->post_author);
+        $author_fb = get_the_author_meta('ab_facebook', $post->post_author);
+        $author_twi = get_the_author_meta('ab_twitter', $post->post_author);
+        $author_link = get_the_author_meta('ab_linkedin', $post->post_author);
+        $author_pic = get_avatar( get_current_user_ID(), 96 );
+
+
+        $author_bio = '<p><strong>Author Bio: </strong>' .'<br>' .$author_pic .'<br>' . esc_html($author_name) .'<br>' .esc_html($author_mail) .'<br>' .esc_html($author_url) .'<br>' .esc_html($author_description) .'<br>' .esc_html($author_fb) .'<br>' .esc_html($author_twi) .'<br>' .esc_html($author_link) . '</p>';
+
+        ob_start();
+        
+       ?>
+
+        <div class="author-box">
+            <div class="img">
+                <?php echo $author_pic;?>
+            </div>
+            <div class="content">
+                <p class="title"><?php echo esc_html($author_name);?></p>
+                <p class="des"><?php echo esc_html($author_description);?></p>
+            </div>
+        </div>
+        
+        
+        <?php
+
+        $author_html = ob_get_clean(); 
+        return $content . $author_html;
+        
     }
 
     return $content;
@@ -26,7 +63,18 @@ function display_author_bio($content){
 
 add_filter('the_content' , 'display_author_bio');
 
+add_filter( 'user_contactmethods', 'modify_user_contact_methods' );
 
+function modify_user_contact_methods( $methods ) {
+
+	
+	$methods['ab_facebook']   = __( 'Facebook' );
+	$methods['ab_twitter'] = __( 'Twitter' );
+    $methods['ab_linkedin']   = __( 'Linkedin' );
+	
+
+	return $methods;
+}
 
 
 
